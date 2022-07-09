3a50b953401b355ffaff652ef48f03efe48736e3
Rachel Ng <rachel.ng.135@gmail.com>
2022-07-09 01:00:07 -0700

    2021

---
+++ complexity.py
@@ -0,0 +1,6 @@
+def complexity(texts): 
+    return np.array([[mean(len(w) for w in wt), len(set(wt)) / len(wt), st, len([w for w in wt if len(w) >= 6]) * 1.0 / len(wt)] for st, wt in 
+            zip(
+                (mean(len([w.lower() for w in nltk.word_tokenize(s) if w not in string.punctuation]) for s in nltk.sent_tokenize(text)) for text in texts), 
+                ([w.lower() for w in nltk.word_tokenize(text) if w not in string.punctuation] for text in texts))
+           ]), ["average number of characters per word", "#unique words / #total words", "average number of words per sentence", 'count of "long" words - words with >= 6 letters']
